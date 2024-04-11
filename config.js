015ef4d989feb5bc33ce5ed75409d37f16688fe3
isalig <ischuk.aleksander@gmail.com>
2024-04-11 15:00:39 +0700

    Initialize renovate configs

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    "group:monorepos",
+    "group:recommended",
+  ],
+  platform: "github",
+  token: process.env.GITHUB_TOKEN,
+  onboardingBranch: "renovate-onboarding",
+  branchPrefix: "renovate/",
+  labels: ["renovate"],
+  stabilityDays: 3,
+  dependencyDashboard: true,
+  semanticCommits: "enabled",
+  requireConfig: true,
+  // Опция для дебага
+  // dryRun: true,
+  // logLevel: 'debug',
+  persistRepoData: true,
+  timezone: "Europe/Moscow",
+  schedule: ["every hour",],
+  rangeStrategy: "auto",
+  packageRules: [
+    {
+      matchUpdateTypes: ["major"],
+      addLabels: ["renovate-major"],
+    },
+    {
+      matchUpdateTypes: ["minor"],
+      addLabels: ["renovate-minor"],
+    },
+    {
+      matchUpdateTypes: ["patch"],
+      addLabels: ["renovate-patch"],
+    },
+    {
+      matchDepTypes: ["dependencies"],
+      addLabels: ["renovate-dependencies"],
+    },
+    {
+      matchDepTypes: ["devDependencies"],
+      addLabels: ["renovate-devDependencies"],
+    }
+  ],
+};
